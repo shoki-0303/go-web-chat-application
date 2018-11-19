a0fa4a21dd51bff753fe2a87aa1c9ebcf1f8ac4e
shoki-0303 <shoki522.33333@gmail.com>
2018-11-20 07:00:17 +0900

    Refactor

--- chat/auth.go
+++ chat/auth.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"net/http"
 )
 
@@ -15,13 +14,11 @@ func mustAuth(h http.Handler) http.Handler {
 
 func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
-		log.Println("未認証")
 		w.Header().Set("Location", "/login")
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	} else if err != nil {
 		panic(err.Error())
 	} else {
-		log.Println("認証通過")
 		h.next.ServeHTTP(w, r)
 	}
 }
