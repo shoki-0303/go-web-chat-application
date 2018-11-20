3924f31c4b26b6a9270b04d1bff7bbf92f9fbe07
shoki-0303 <shoki522.33333@gmail.com>
2018-11-20 09:27:45 +0900

    Add providers

--- chat/main.go
+++ chat/main.go
@@ -5,8 +5,19 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/stretchr/gomniauth/providers/google"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/signature"
+)
+
+var (
+	googleClientID     = os.Getenv("Google_Client_ID")
+	googleClientSecret = os.Getenv("Google_Client_Secret")
 )
 
 type templateHandler struct {
@@ -26,9 +37,14 @@ func main() {
 	r := newRoom()
 	addr := flag.String("addr", ":8080", "address of application")
 	flag.Parse()
+	gomniauth.SetSecurityKey(signature.RandomKey(64))
+	gomniauth.WithProviders(
+		google.New(googleClientID, googleClientSecret, "http://localhost:8080/auth/callback/google"),
+	)
 	http.Handle("/chat", mustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/room", r)
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	go r.run()
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatalln("ListenAndServe error:", err)
