2769639e79b1960fa0372c5b3a094054ed8797cf
shoki-0303 <shoki522.33333@gmail.com>
2018-11-20 21:01:47 +0900

    add AuthAvatar

---
+++ chat/avatar.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"errors"
+)
+
+// ErrNoAvatar : ErrNoAvatar returns error, when avatar instance can't return url.
+var ErrNoAvatar = errors.New("avatar-url doesn't exist")
+
+// Avatar : avatar instance has a GetAvatar method
+type Avatar interface {
+	GetAvatar(c *client) (string, error)
+}
+
+// AuthAvatar : get avatar via auth
+type AuthAvatar struct{}
+
+// GetAvatar : get avatar-url
+func (*AuthAvatar) GetAvatar(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatar
+}
