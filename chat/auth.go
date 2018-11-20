2e893dc70af11f73b1e11ce59e2574da2f9d010a
shoki-0303 <shoki522.33333@gmail.com>
2018-11-20 09:27:26 +0900

    add login of loginHandler

--- chat/auth.go
+++ chat/auth.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"strings"
+
+	"github.com/stretchr/gomniauth"
 )
 
 type authHandler struct {
@@ -20,5 +25,33 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		panic(err.Error())
 	} else {
 		h.next.ServeHTTP(w, r)
+	}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+
+	switch action {
+	case "login":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Println("login-provider error", err)
+			return
+		}
+		url, err := provider.GetBeginAuthURL(nil, nil)
+		if err != nil {
+			log.Println("login-geturl error", err)
+			return
+		}
+
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	case "callback":
+		fmt.Println("callback時の処理を記述")
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		log.Printf("%sという誤ったアクションを検出しました", action)
 	}
 }
