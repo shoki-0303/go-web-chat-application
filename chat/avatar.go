e7b8385d42e997e23ecb29b610ae6bd0a6073511
shoki-0303 <shoki522.33333@gmail.com>
2018-11-21 21:17:50 +0900

    add GravatarAvatar

--- chat/avatar.go
+++ chat/avatar.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"crypto/md5"
 	"errors"
+	"fmt"
+	"io"
+	"strings"
 )
 
 // ErrNoAvatar : ErrNoAvatar returns error, when avatar instance can't return url.
@@ -23,6 +27,26 @@ func (AuthAvatar) GetAvatar(c *client) (string, error) {
 	if url, ok := c.userData["avatar_url"]; ok {
 		if urlStr, ok := url.(string); ok {
 			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatar
+}
+
+// GravatarAvatar : get avatar via gravatar
+type GravatarAvatar struct{}
+
+// UseGravatarAvatar : instance of GravatarAvatar
+var UseGravatarAvatar GravatarAvatar
+
+// GetAvatar : get avatar-url
+func (GravatarAvatar) GetAvatar(c *client) (string, error) {
+	if email, ok := c.userData["email"]; ok {
+		if emailStr, ok := email.(string); ok {
+			emailStrLow := strings.ToLower(emailStr)
+			hasher := md5.New()
+			io.WriteString(hasher, emailStrLow)
+			url := fmt.Sprintf("https://www.gravatar.com/avatar/%x", hasher.Sum(nil))
+			return url, nil
 		}
 	}
 	return "", ErrNoAvatar
