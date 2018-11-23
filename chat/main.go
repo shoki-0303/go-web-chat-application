94f08a12eff9533529c2b294261ab987938a5d22
shoki-0303 <shoki522.33333@gmail.com>
2018-11-23 15:24:50 +0900

    use FileSystemAvatar

--- chat/main.go
+++ chat/main.go
@@ -22,7 +22,7 @@ var (
 	googleClientSecret = os.Getenv("Google_Client_Secret")
 )
 
-var avatar Avatar = UseGravatarAvatar
+var avatar Avatar = UseFileSystemAvatar
 
 type templateHandler struct {
 	once     sync.Once
@@ -57,6 +57,7 @@ func main() {
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/upload", &templateHandler{filename: "upload.html"})
 	http.HandleFunc("/uploader", uploaderHandler)
+	http.Handle("/avatars/", http.StripPrefix("/avatars/", http.FileServer(http.Dir("./avatars"))))
 	go r.run()
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatalln("ListenAndServe error:", err)
